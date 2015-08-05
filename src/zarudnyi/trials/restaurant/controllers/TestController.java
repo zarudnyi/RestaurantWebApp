9994e4587b9b4bbf4982dc6d79d4e67df069f18c
ivan-zarudnyi <zarudnyi.ivan@gmail.com>
2015-08-05 16:27:34 +0300

    .

--- src/zarudnyi/trials/restaurant/controllers/TestController.java
+++ src/zarudnyi/trials/restaurant/controllers/TestController.java
@@ -1,6 +1,5 @@
 package zarudnyi.trials.restaurant.controllers;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +10,6 @@ import zarudnyi.trials.restaurant.config.AppConfig;
 public class TestController {
 
 
-    @Secured("ROLE_TEST")
     @RequestMapping(value = {"/reset"}, method = {RequestMethod.GET})
     ModelAndView profilePage(){
         ModelAndView model = new ModelAndView();
